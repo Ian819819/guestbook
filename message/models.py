c8c07b88a3228a590a25a903cf4b0bc56c621103
Ian <ianlai819819@gmail.com>
2024-07-04 11:16:15 +0800

    定義資料模型

--- message/models.py
+++ message/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Message(models.Model):
-    user = models.CharField("姓名", max_length=50)
-    subject = models.CharField("主旨", max_length=200)
-    content = models.TextField("內容")
-    created = models.DateTimeField("留言時間", auto_now_add=True)
-
+    user = models.CharField('留言者', max_length=50)
+    receipt = models.CharField('收件人', max_length=60)
+    subject = models.CharField('留言主題', max_length=128)
+    content = models.TextField('留言內容')
+    created = models.DateTimeField('留言時間', auto_now_add=True)
+      
     def __str__(self):
         return self.subject
